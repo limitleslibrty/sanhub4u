7256b08aeaec561b2728223fab3add4181a0becc
limitleslibrty <jkvk27753@gmail.com>
2025-10-25 16:08:05 +0530

    removed js and button tempelate file

--- main.js
+++
@@ -1,9 +0,0 @@
-function searchMovies() {
-    let input = document.getElementById("searchBox").value.toLowerCase();
-    let cards = document.querySelectorAll(".movie-card");
-
-    cards.forEach(card => {
-        let title = card.querySelector(".movie-title").textContent.toLowerCase();
-        card.style.display = title.includes(input) ? "inline-block" : "none";
-    });
-}
